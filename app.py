83fb287fd15cf97131b5399c44fd7c42d30719e6
Brad Swan <bradj.swan@gmail.com>
2020-08-31 12:57:56 +1000

    luminosity matrix

--- app.py
+++ app.py
@@ -11,11 +11,35 @@ def generate_pixel_array():
         w = 1
         line_array = []
         while w < width:
-            line_array.append(im.getpixel((w, 10)))
+            line_array.append(im.getpixel((w, h)))
             w += 1
         h += 1
         full_array.append(line_array)
     return full_array
 
+def construct_brightness_matrix(rgb_array):
+    new_array = []
+    for h in rgb_array:
+        line_array = []
+        for w in h:
+            red = w[0] * 0.21
+            green = w[1] * 0.72
+            blue = w[2] * 0.07
+            lum = red + green + blue
+            w = round(lum)
+            line_array.append(w)
+        new_array.append(line_array)
+    print("Successfully constructed brightness matrix!")
+    print(f"Brightness matrix size {width} x {height}")
+    return new_array
+
+
+
 array = generate_pixel_array()
-print(array)
+rgb_array = construct_brightness_matrix(array)
+
+#print(rgb_array)
+
+chars = "`^\\\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+
+print(chars)
